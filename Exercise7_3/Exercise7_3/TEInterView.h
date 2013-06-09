b01cb47107cfe92f18c48a3d5795058fb5c7ec98
Tuan Nguyen Anh <gr3man@gmail.com>
2013-06-09 16:54:43 +0700

    Draw Popupview, Create a UIView for contain popup

---
+++ Exercise7_3/Exercise7_3/TEInterView.h
@@ -0,0 +1,13 @@
+//
+//  TEInterView.h
+//  Exercise7_3
+//
+//  Created by Tuan Nguyen Anh on 6/8/13.
+//  Copyright (c) 2013 Tuấn Nguyễn Anh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEInterView : UIView
+
+@end
---
+++ Exercise7_3/Exercise7_3/TEInterView.m
@@ -0,0 +1,31 @@
+//
+//  TEInterView.m
+//  Exercise7_3
+//
+//  Created by Tuan Nguyen Anh on 6/8/13.
+//  Copyright (c) 2013 Tuấn Nguyễn Anh. All rights reserved.
+//
+
+#import "TEInterView.h"
+
+@implementation TEInterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
