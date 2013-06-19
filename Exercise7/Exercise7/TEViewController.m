9f5807c9de27e21286030ab627f464f3e980300a
gr3man <gr3man@gmail.com>
2013-06-19 10:44:46 +0700

    1234

--- Exercise7/Exercise7/TEViewController.m
+++ Exercise7/Exercise7/TEViewController.m
@@ -22,7 +22,7 @@ int numberDate[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"123");
+    NSLog(@"1234");
 	// Do any additional setup after loading the view, typically from a nib.
     NSDate *today = [NSDate date];
     
